7df647d084387d026544a019f8288ca0cfb7763d
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2024-10-25 13:54:39 -0400

    Update convert.js

--- convert.js
+++ convert.js
@@ -1,12 +1,27 @@
 function binaryToDecimal(input){
     let deci = 0;
-    binaryString = String(input);
-    const n = binaryString.length
-    for(let i=0; i>n; i++){
-        let power = 2**(n-(i+1));
-        deci += Number(binaryString[i]) * power;
+    for(let i=0; i<input.length; i++){
+        if (input[input.length-i-1]==="1"){
+            deci = deci + Math.pow(2,i);
+        }
     }
     return deci;
 };
 
-console.log(binaryToDecimal("1001"));
+console.log(binaryToDecimal("100101"));
+
+function deciToBinary(input){
+    let binary = "";
+    while(input>=1){
+        let r = input % 2
+        input = Math.floor(input / 2)
+        if ((r === 1)){
+            binary = binary + r;
+        }else{
+            binary = binary + "0";
+        }
+    };
+    return binary;
+};
+
+console.log(deciToBinary(9));
